cce90e4980d034adb044a39befa154224515c19a
Paul Gey <narpfel@gmx.de>
2016-05-25 19:07:56 +0200

    Implement bitboard representation for ConnectFour

--- src/connect_four.rs
+++ src/connect_four.rs
@@ -309,6 +309,122 @@ impl fmt::Display for VecState {
 }
 
 
+#[derive(Clone, Debug)]
+pub struct BitState {
+    state: [BitBoard; 2],
+    empty_per_column: [u8; 12],
+    columns: u8,
+    rows: u8,
+    last_player: Player,
+    last_column: u8,
+}
+
+impl State for BitState {
+    fn new(columns: usize, rows: usize) -> Result<Self, ()> {
+        if columns * (rows + 1) > 64 || columns > 12 {
+            Err(())
+        }
+        else {
+            Ok(
+                BitState {
+                    state: [BitBoard(0), BitBoard(0)],
+                    empty_per_column: [rows as u8 - 1; 12],
+                    columns: columns as u8,
+                    rows: rows as u8,
+                    // last_move: (0, 0),
+                    last_player: Player(0),
+                    last_column: 255,
+                }
+            )
+        }
+    }
+
+    fn size(&self) -> (usize, usize) {
+        (self.columns as usize, self.rows as usize)
+    }
+
+    fn play(&mut self, column: usize, player: Player) -> Result<(), InvalidMove> {
+        try!(self.validate_move(column));
+        let row = self.empty_per_column[column] as usize;
+        self.set(column, row, player);
+        self.empty_per_column[column] -= 1;
+        self.last_player = player;
+        self.last_column = column as u8;
+        Ok(())
+    }
+
+    fn has_just_won(&self) -> bool {
+        let Player(p) = self.last_player;
+        self.state[p as usize - 1].has_winner(self.rows)
+    }
+
+    fn set(&mut self, column: usize, row: usize, Player(player): Player) {
+        self.state[player as usize - 1].set_bit(column as u8, row as u8, self.rows);
+    }
+
+    fn get(&self, column: usize, row: usize) -> Player {
+        Player(
+            2 * self.state[1].get_bit(column as u8, row as u8, self.rows)
+            + self.state[0].get_bit(column as u8, row as u8, self.rows)
+        )
+    }
+
+    fn valid_moves(&self) -> Vec<usize> {
+        // Cannot `filter()` and `collect()` here as `Filter::size_hint()` returns a lower bound
+        // of 0, which means the `Vec` has to realloc several times.
+        let mut v = Vec::with_capacity(self.columns as usize);
+        for (i, &empty) in self.empty_per_column.iter().take(self.columns as usize).enumerate() {
+            if empty != 0 {
+                v.push(i);
+            }
+        }
+        v
+    }
+
+    fn last_move(&self) -> (usize, usize) {
+        (self.last_column as usize, self.empty_per_column[self.last_column as usize] as usize + 1)
+    }
+}
+
+impl fmt::Display for BitState {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self._fmt(f)
+    }
+}
+
+
+#[derive(Copy, Clone, Debug)]
+pub struct BitBoard(u64);
+
+impl BitBoard {
+    #[inline]
+    fn has_winner(&self, rows: u8) -> bool {
+        let x = self.0;
+        let vertical = x & x >> 1;
+        let horizontal = x & x >> rows + 1;
+        let diagonal_ud = x & x >> rows;
+        let diagonal_du = x & x >> rows + 2;
+
+        (vertical & vertical >> 2)
+        | (horizontal & horizontal >> 2 * (rows + 1))
+        | (diagonal_du & diagonal_du >> 2 * rows)
+        | (diagonal_ud & diagonal_ud >> 2 * (rows + 2))
+        != 0
+    }
+
+
+    #[inline(always)]
+    fn get_bit(&self, column: u8, row: u8, rows: u8) -> u8 {
+        ((self.0 >> (column * (rows + 1) + row)) & 1) as u8
+    }
+
+    #[inline(always)]
+    fn set_bit(&mut self, column: u8, row: u8, rows: u8) {
+        self.0 |= 1 << (column * (rows + 1) + row);
+    }
+}
+
+
 #[derive(Debug)]
 pub enum InvalidMove {
     InvalidColumn(usize),
