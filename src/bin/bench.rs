54b556644dd27b13f948df89a4d784548c63ab60
Paul Gey <narpfel@gmx.de>
2018-01-17 19:25:54 +0100

    Move conversion from `Duration` to seconds to function

--- src/bin/bench.rs
+++ src/bin/bench.rs
@@ -1,9 +1,18 @@
 extern crate mcc4;
 
-use std::time::Instant;
+use std::time::{Instant, Duration};
 
 use mcc4::*;
 use mcc4::ai_player::SIMULATIONS;
+
+
+pub const NANOS_PER_SECOND: u64 = 1_000_000_000;
+
+
+fn as_fractional_secs(duration: Duration) -> f64 {
+    (duration.as_secs() * NANOS_PER_SECOND + duration.subsec_nanos() as u64) as f64
+        / NANOS_PER_SECOND as f64
+}
 
 
 fn main() {
@@ -12,8 +21,7 @@ fn main() {
     let benchmark_player = AiPlayer::new();
     let now = Instant::now();
     benchmark_player.make_move(&game);
-    let time = now.elapsed();
-    let seconds = (time.as_secs() * 1_000_000_000 + time.subsec_nanos() as u64) as f64 / 1_000_000_000.;
+    let seconds = as_fractional_secs(now.elapsed());
     println!("{} seconds elapsed", seconds);
     println!("{:?} games per second", (columns * SIMULATIONS) as f64 / seconds);
 }
