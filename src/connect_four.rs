5dd7d6d02487d156e142822be2524a71e6b9eb2a
Paul Gey <narpfel@gmx.de>
2018-12-21 02:09:48 +0100

    Remove displaying and `Moves`

--- src/connect_four.rs
+++ src/connect_four.rs
@@ -1,7 +1,3 @@
-use std::fmt;
-
-use crate::ai_player::MonteCarloPlayer;
-
 #[derive(Debug, Clone)]
 pub struct ConnectFour {
     state: BitState,
@@ -29,10 +25,6 @@ impl ConnectFour {
         else {
             Ok(None)
         }
-    }
-
-    fn winner(&self) -> Option<Player> {
-        self.winner
     }
 
     pub fn valid_moves(&self) -> Vec<Move> {
@@ -64,76 +56,12 @@ impl ConnectFour {
         let Player(p) = self.current_player();
         Player(3 - p)
     }
-
-    fn has_ended(&self) -> bool {
-        self.valid_moves().is_empty() || self.winner().is_some()
-    }
-
-    pub fn iter(self, players: Vec<MonteCarloPlayer>) -> Moves {
-        Moves::new(self, players)
-    }
-}
-
-pub struct Moves {
-    game: ConnectFour,
-    players: Vec<MonteCarloPlayer>,
-    current_player_index: usize,
-}
-
-impl Moves {
-    fn new(game: ConnectFour, players: Vec<MonteCarloPlayer>) -> Moves {
-        Moves {
-            game: game,
-            players: players,
-            current_player_index: 0,
-        }
-    }
-}
-
-impl Iterator for Moves {
-    type Item = (BitState, Player, Move, Winner);
-
-    #[inline(always)]
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.game.has_ended() || self.game.valid_moves().is_empty() {
-            return None;
-        }
-
-        let num_players = self.players.len();
-        let player = self.game.current_player();
-
-        loop {
-            let move_ = self.players[self.current_player_index].make_move(&self.game);
-            match self.game.play(move_) {
-                Ok(maybe_winner) => {
-                    self.current_player_index = (self.current_player_index + 1) % num_players;
-                    let winner = match maybe_winner {
-                        Some(winner) => Winner::Winner(winner),
-                        None => if self.game.has_ended() { Winner::Draw } else { Winner::NotFinishedYet }
-                    };
-                    return Some((self.game.state().clone(), player, move_, winner));
-                },
-                Err(invalid_move) => {
-                    self.players[self.current_player_index].invalid_move(invalid_move);
-                }
-            }
-        }
-    }
 }
 
 pub type Move = usize;
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub struct Player(pub u8);
-
-impl fmt::Display for Player {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f, "{}\x1B[0m",
-            if self.0 == 1 { "\x1B[44;1mX" } else if self.0 == 2 { "\x1B[41;1mO" } else { " " }
-        )
-    }
-}
 
 
 #[derive(Debug)]
@@ -219,27 +147,6 @@ impl BitState {
         moves
     }
 
-    fn _fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let (n_columns, n_rows) = self.size();
-        let fill_row = |left, joiner, right| {
-            format!("{}{}{}", left, vec!["─"; n_columns].join(joiner), right)
-        };
-
-        let rows: Vec<_> = (0..n_rows).map(|i| {
-            let row = self.row(i).unwrap_or_else(|| unreachable!());
-            let positions: Vec<_> = row.iter().map(|p| format!("{}", p)).collect();
-            format!("│{}│\n", positions.join("│"))
-        }).collect();
-
-        write!(
-            f, "{top_row}{body}{bottom_row}",
-            top_row = fill_row("┌", "┬", "┐\n"),
-            body = rows.join(&fill_row("├", "┼", "┤\n")),
-            bottom_row = fill_row("└", "┴", "┘\n")
-        )?;
-        write!(f, " {}\n", (0..n_columns).map(|n| format!("{}", n)).collect::<Vec<_>>().join(" "))
-    }
-
     fn validate_move(&self, column_number: usize) -> Result<(), InvalidMove> {
         let max_column = self.size().0;
         if column_number >= max_column {
@@ -252,25 +159,7 @@ impl BitState {
             Ok(())
         }
     }
-
-    fn row(&self, row: usize) -> Option<Box<[Player]>> {
-        if row >= self.size().1 {
-            return None;
-        }
-        Some(
-            (0..self.size().0)
-            .map(|column| self.get(column, row))
-            .collect::<Vec<_>>().into_boxed_slice()
-        )
-    }
 }
-
-impl fmt::Display for BitState {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        self._fmt(f)
-    }
-}
-
 
 #[derive(Copy, Clone, Debug)]
 pub struct BitBoard(u64);
